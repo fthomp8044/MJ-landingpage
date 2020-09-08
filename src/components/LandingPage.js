4623c43a2b5dd5fdbe7018fa9f2caa08ac533df3
fthomp8044 <fthomp2430@gmail.com>
2020-09-08 19:03:41 -0400

    created landing page background

---
+++ src/components/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+      <div className="container-fluid" >
+        <button></button>
+      </div>
+  )
+};
+
+export default LandingPage;
